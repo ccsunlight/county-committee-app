e8764d361cea00f7b7fa4b7acfdb3611e89599ca
Jonathan Crockett <jonathanc@complex.com>
2017-06-15 01:45:48 -0400

    Bug fix prod domain hostname for admin

--- public/cc-admin/src/feathersClient.js
+++ public/cc-admin/src/feathersClient.js
@@ -3,7 +3,9 @@ import hooks from 'feathers-hooks';
 import rest from 'feathers-rest/client';
 // import authentication from 'feathers-authentication-client';
 
-const host = 'http://localhost';
+const host = '//' + window.location.hostname;
+
+console.log( window.location.hostname);
 
 export default feathers()
   .configure(hooks())
