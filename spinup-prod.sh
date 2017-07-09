2b8cd7a0292cb5d0869a69f5305b3b642904b571
Jonathan Crockett <jonathanc@complex.com>
2017-07-09 18:33:59 -0400

    Bug fixes pm2

--- spinup-prod.sh
+++ spinup-prod.sh
@@ -9,4 +9,4 @@ docker run --rm -it --name cc-mongo -p 27017:27017 -v cc-db-test-data:/data/db -
 # you can ignore error from this command
 docker stop cc-app || true && docker rm cc-app || true
 
-docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app --link=cc-mongo:mongodb -e MONGODB_PORT_27017_TCP_ADDR="172.17.0.2" -p 25:25 -p 587:587 -p 4000:80 -p 465:465 -e "NODE_ENV=production" --name cc-app -d node:6.10.0 sh -c "npm install ; ./node_modules/.bin/pm2 start src --name=app" 
+docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app --link=cc-mongo:mongodb -e MONGODB_PORT_27017_TCP_ADDR="172.17.0.2" -p 25:25 -p 587:587 -p 4000:80 -p 465:465 -e "NODE_ENV=production" --name cc-app -d node:6.10.0 sh -c "npm install ; ./node_modules/.bin/pm2 start src --name=app --no-daemon" 
