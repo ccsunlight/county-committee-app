61cc85f1c3aeca28d83ed9d2d5c9ae134d3f20e5
Jonathan Crockett <jonathanc@complex.com>
2017-12-04 14:56:23 -0500

    Icon updates.

--- public/cc-admin/src/App.js
+++ public/cc-admin/src/App.js
@@ -5,6 +5,8 @@ import { Admin, Resource, Delete, Create, Edit, List } from 'admin-on-rest';
 import restClient from './feathersRestClient';
 //import { WithPermission } from 'aor-permissions';
 
+import GlossaryTermIcon from 'material-ui/svg-icons/action/speaker-notes';
+import ActionLogIcon from 'material-ui/svg-icons/device/storage';
 import PostIcon from 'material-ui/svg-icons/action/book';
 import UserIcon from 'material-ui/svg-icons/social/group';
 import PersonAddIcon from 'material-ui/svg-icons/social/person-add';
@@ -48,9 +50,9 @@ const App = () => (
 
       <Resource name="invite" icon={PersonAddIcon} list={InviteList} create={InviteCreate} remove={Delete} />
 
-      <Resource name="glossary-term" options={{ label: 'Glossary' }}   icon={PersonAddIcon} list={GlossaryTermList} create={GlossaryTermCreate} edit={GlossaryTermEdit} remove={Delete} />
+      <Resource name="glossary-term" options={{ label: 'Glossary' }}   icon={GlossaryTermIcon} list={GlossaryTermList} create={GlossaryTermCreate} edit={GlossaryTermEdit} remove={Delete} />
 
-      <Resource name="action-log" options={{ label: 'Action Log' }} icon={PostIcon} list={ActionLogList}  />
+      <Resource name="action-log" options={{ label: 'Action Log' }} icon={ActionLogIcon} list={ActionLogList}  />
       
     </Admin>
 );
