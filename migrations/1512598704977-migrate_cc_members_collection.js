51e0c9f62bd5b120f4c87ebb1965bdbc62bb5105
Jonathan Crockett <jonathanc@complex.com>
2017-12-06 18:20:47 -0500

    Adding migration script

---
+++ migrations/1512598704977-migrate_cc_members_collection.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// Changing the name of "county-committees" to "county-committee-members".
+// County committees should be moved to "county-committees"
+
+/**
+ * Make any changes you need to make to the database here
+ */
+exports.up = function up (done) {
+
+	const mongoose   = require('mongoose');
+	mongoose.Promise = Promise;
+
+	mongoose.connect('mongodb://172.17.0.2:27017/county-committee').then(() => {
+	  console.log('connected');
+
+	  // Access the underlying database object provided by the MongoDB driver.
+	  let db = mongoose.connection.db;
+
+	  // Rename the `test` collection to `foobar`
+	  return db.collection('county-committees').rename('county-committee-members');
+	
+	}).then(() => {
+	  console.log('rename successful');
+	}).catch(e => {
+	  console.log('rename failed:', e.message);
+	}).then(() => {
+	  console.log('disconnecting');
+	  mongoose.disconnect();
+	});
+
+  done();
+};
+
+/**
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+exports.down = function down(done) {
+  done();
+};
