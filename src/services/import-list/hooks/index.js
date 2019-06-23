1324362b4152dd5869abb0862642888513fcfb92
Jonathan Crockett <jonathanc@complex.com>
2019-06-23 16:21:48 -0400

    Renamed appointed service to import list

---
+++ src/services/import-list/hooks/index.js
@@ -0,0 +1,88 @@
+"use strict";
+
+const globalHooks = require("../../../hooks");
+const hooks = require("feathers-hooks");
+const CountyCommitteeMemberModel = require("../../county-committee-member/county-committee-member-model");
+const AppointedListModel = require("../import-list-model");
+
+exports.before = {
+  all: [],
+  find: [
+    // Excludes the "positions" property from finds
+    // to avoid memory overload of returning
+    // 1000+ array.
+    // @todo move positions to their own collection
+    function(context) {
+      context.params.query.$select = { positions: 0 };
+
+      return context;
+    }
+  ],
+  get: [],
+  create: [savePartyCallJsonDataCSV],
+  update: [savePartyCallJsonDataCSV],
+  patch: [savePartyCallJsonDataCSV],
+  remove: []
+};
+
+exports.after = {
+  all: [],
+  find: [],
+  get: [],
+  create: [globalHooks.logAction],
+  update: [globalHooks.logAction],
+  patch: [
+    function(hook) {
+      // Only import list if members have not been imported.
+      if (hook.result.isApproved && !hook.result.isImported) {
+        const membersToImport = hook.result.members;
+
+        membersToImport.forEach((ccMember, index) => {
+          CountyCommitteeMemberModel.create(ccMember, function(
+            err,
+            addedMember
+          ) {
+            if (err) console.error(err);
+
+            // If all members are imported, set the flag to true.
+            if (index === membersToImport.length - 1) {
+              AppointedListModel.findByIdAndUpdate(
+                hook.result._id,
+                { isImported: true },
+                {},
+                success => {}
+              );
+            }
+          });
+        });
+      }
+    },
+    globalHooks.logAction
+  ],
+  remove: [globalHooks.logAction]
+};
+
+/**
+ * Checks for a data json string for party call and if present uses that
+ * to create the party call. For use with JSON rest POST requests.
+ *
+ * @param {Object} context The hook context
+ * @return {Object} The modified hook context
+ */
+function savePartyCallJsonDataCSV(context) {
+  if (context.data.hasOwnProperty("file_data")) {
+    let csvBase64DataObject = context.data.file_data.pop();
+    if (csvBase64DataObject) {
+      let csvFileTempFilePath = context.app
+        .service("utils")
+        .saveBase64CSVDataToTempFile(
+          csvBase64DataObject.src,
+          csvBase64DataObject.title
+        );
+
+      context.data.filepath = csvFileTempFilePath;
+    }
+  }
+
+  return context;
+}
--- src/services/term/index.js
+++ src/services/term/index.js
@@ -4,6 +4,7 @@ const FeathersMongoose = require("feathers-mongoose");
 const TermModel = require("./term-model");
 const MemberModel = require("../county-committee-member/county-committee-member-model");
 const hooks = require("./hooks");
+const mongoose = require("mongoose");
 
 class Service extends FeathersMongoose.Service {
   createMembersFromCertifiedList(params) {
@@ -31,55 +32,51 @@ class Service extends FeathersMongoose.Service {
 
   /**
    * Appoints members to a term
-   * @param {Members} appointees
+   * @param {Members} members
    * @param {Term} term
+   * @param {Object} options
+   * @param {Object} bulkFields Fields that will apply to allrecords
    *
    * @todo Handle asyncronous save better
    */
-  appointMembersToTerm(appointees, term) {
-    const appointedMembers = [];
-    const appointmentErrors = [];
+  importMembersToTerm(members, term, options = {}) {
+    const importedMembers = [];
+    const importErrors = [];
 
     return new Promise((resolve, reject) => {
-      appointees.forEach((appointee, index) => {
-        MemberModel.findOne({
-          electoral_district: appointee.electoral_district,
-          assembly_district: appointee.assembly_district,
-          office: appointee.office,
-          office_holder: "Vacancy",
-          term_id: term._id
-        }).then(async function(member) {
-          if (!member) {
-            appointmentErrors.push(
-              `Appointment failed: row ${index} ED: ${
-                appointee.electoral_district
-              } AD: ${appointee.assembly_district} OFFICE_HOLDER: ${
-                appointee.office_holder
-              }. Check that the position exists or is already filled`
-            );
-          } else {
-            member.office_holder = appointee.office_holder;
-            member.sex = appointee.sex;
-            member.entry_type = appointee.entry_type;
-            member.part = appointee.type;
-            member.address = appointee.address;
-            member.county = appointee.county;
-            member.data_source = appointee.data_source;
-            member.state = appointee.state;
-            appointedMembers.push(member);
-          }
-
-          if (index === appointees.length - 1) {
-            if (appointmentErrors.length > 0) {
-              reject(appointmentErrors);
-            } else {
-              for (let x = 0; x < appointedMembers.length; x++) {
-                await appointedMembers[x].save();
-              }
-              resolve(appointedMembers);
+      var bulkWriteOp = MemberModel.collection.initializeOrderedBulkOp();
+      const bulkFields = { ...options.bulkFields };
+      members.forEach((member, index) => {
+        bulkWriteOp
+          .find({
+            electoral_district: member.electoral_district,
+            assembly_district: member.assembly_district,
+            office: member.office,
+            party: member.party,
+            term_id: term._id
+          })
+          .upsert()
+          .updateOne({
+            $set: {
+              office_holder: member.office_holder,
+              sex: member.sex,
+              part: member.type,
+              address: member.address,
+              county: member.county,
+              data_source: member.data_source,
+              state: member.state,
+              committee: term.committee._id,
+              petition_number: member.petition_number,
+              ...bulkFields
             }
-          }
-        });
+          });
+      });
+      bulkWriteOp.execute(function(err, bulkWriteResult) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(bulkWriteResult);
+        }
       });
     });
   }
