b3f5c5d9cf9e21e542a9dee46d2fb1a1cdaedb64
Jonathan Crockett <jonathanc@complex.com>
2019-06-22 22:40:54 -0400

    Adding appointMembersToTerm

--- src/services/term/index.js
+++ src/services/term/index.js
@@ -7,7 +7,6 @@ const hooks = require("./hooks");
 
 class Service extends FeathersMongoose.Service {
   createMembersFromCertifiedList(params) {
-
     return new Promise((resolve, reject) => {
       TermModel.findOne(params.term_id).then(term => {
         let positions = term.certified_list.positions;
@@ -29,6 +28,61 @@ class Service extends FeathersMongoose.Service {
       });
     });
   }
+
+  /**
+   * Appoints members to a term
+   * @param {Members} appointees
+   * @param {Term} term
+   *
+   * @todo Handle asyncronous save better
+   */
+  appointMembersToTerm(appointees, term) {
+    const appointedMembers = [];
+    const appointmentErrors = [];
+
+    return new Promise((resolve, reject) => {
+      appointees.forEach((appointee, index) => {
+        MemberModel.findOne({
+          electoral_district: appointee.electoral_district,
+          assembly_district: appointee.assembly_district,
+          office: appointee.office,
+          office_holder: "Vacancy",
+          term_id: term._id
+        }).then(async function(member) {
+          if (!member) {
+            appointmentErrors.push(
+              `Appointment failed: row ${index} ED: ${
+                appointee.electoral_district
+              } AD: ${appointee.assembly_district} OFFICE_HOLDER: ${
+                appointee.office_holder
+              }. Check that the position exists or is already filled`
+            );
+          } else {
+            member.office_holder = appointee.office_holder;
+            member.sex = appointee.sex;
+            member.entry_type = appointee.entry_type;
+            member.part = appointee.type;
+            member.address = appointee.address;
+            member.county = appointee.county;
+            member.data_source = appointee.data_source;
+            member.state = appointee.state;
+            appointedMembers.push(member);
+          }
+
+          if (index === appointees.length - 1) {
+            if (appointmentErrors.length > 0) {
+              reject(appointmentErrors);
+            } else {
+              for (let x = 0; x < appointedMembers.length; x++) {
+                await appointedMembers[x].save();
+              }
+              resolve(appointedMembers);
+            }
+          }
+        });
+      });
+    });
+  }
 }
 
 module.exports = function() {
@@ -38,7 +92,7 @@ module.exports = function() {
     Model: TermModel,
     paginate: {
       default: 5,
-      max: app.get('api').defaultItemLimit
+      max: app.get("api").defaultItemLimit
     },
     lean: false
   };
